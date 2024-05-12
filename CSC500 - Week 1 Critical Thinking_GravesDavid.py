5fd11f5a1c927df8162248ac591bd87d42d56d32
diggerwoods <digger.woods@gmail.com>
2024-05-12 14:47:25 -0600

    Create CSC500 - Week 1 Critical Thinking_GravesDavid.py

---
+++ CSC500 - Week 1 Critical Thinking_GravesDavid.py
@@ -0,0 +1,29 @@
+# Critical Thinking Week 1
+
+# Part 1: Write a Python program to find the addition and subtraction of two numbers.
+    #Ask the user to input two numbers (num1 and num2). Given those two numbers, add them together to find the output. 
+    #Also, subtract the two numbers to find the output.
+
+# Step 1: user input two numbers (num1 and num2)
+num1 = int(input('Enter First Number: '))
+num2 = int(input('Enter Second Number: '))
+
+# Step 2: calculate sum and display
+print('Sum of both numbers =', num1+num2)
+
+# Step 3: calculate difference and display
+print('Difference of both numbers =', num1-num2)
+
+# Part 2: Write a Python program to find the multiplication and division of two numbers.
+    #Ask the user to input two numbers (num1 and num2). Given those two numbers, multiply them together to find the output. 
+    #Also, divide num1/num2 to find the output.
+
+# Step 1: user input two numbers (num1 and num2)
+num1 = int(input('Enter First Number: '))
+num2 = int(input('Enter Second Number: '))
+
+# Step 2: calculate product and display
+print('Product of both numbers =', num1*num2)
+
+# Step 3: calculate quotient and display
+print('Quotient of both numbers =', num1/num2)
