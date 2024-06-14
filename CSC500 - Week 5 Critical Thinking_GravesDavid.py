b9ecc6164499e5bc4402c6395328ad34b6699c1f
diggerwoods <digger.woods@gmail.com>
2024-06-14 14:41:45 -0600

    Update CSC500 - Week 5 Critical Thinking_GravesDavid.py

--- CSC500 - Week 5 Critical Thinking_GravesDavid.py
+++ CSC500 - Week 5 Critical Thinking_GravesDavid.py
@@ -1,20 +1,20 @@
 # Part 1
 def main():
-    # Ask user for the number of years
+    # Step 1: Ask user for the number of years
     num_years = int(input("Enter the number of years: "))
 
     total_rainfall = 0
     num_months = num_years * 12
 
-    # Outer loop for each year
+    # Step 2: Create a loop for each year
     for year in range(1, num_years + 1):
         print(f"Year {year}:")
         for month in range(1, 13):
-            # ask user for the inches of rainfall for the current month
+            # Step 3: Ask user for the inches of rainfall for the current month
             rainfall = float(input(f"Enter rainfall for month {month} (in inches): "))
             total_rainfall += rainfall
 
-    # Calculate average rainfall
+    # Step 4: Calculate and print average rainfall
     average_rainfall = total_rainfall / num_months
 
     print("\nResults:")
@@ -45,7 +45,7 @@ else:
         message += "You have received 15 points this month."
     elif 6 <= number_of_books <= 7:
         message += "You have received 30 points this month."
-    else:
+    else: 8 <= number_of_books
         message += "You have received 60 points this month."
 
 # Display the result
